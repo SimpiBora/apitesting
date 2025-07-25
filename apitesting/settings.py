3f19cee11184d22542470e0185e867c949d83e4c
simi <simiprinters049@gmail.com>
2025-07-25 14:36:14 +0530

    i dont knwo whats going on

--- apitesting/settings.py
+++ apitesting/settings.py
@@ -22,7 +22,7 @@ DEBUG = os.environ.get("DEBUG")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "*").split(",")
-ALLOWED_HOSTS = ['web-production-9fcc5.up.railway.app']
+ALLOWED_HOSTS = ['web-production-9fcc5.up.railway.app','*']
 
 
 # ALLOWED_HOSTS = []
